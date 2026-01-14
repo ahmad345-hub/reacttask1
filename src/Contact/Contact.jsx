b2b3f7b1e3d0e8bf0077a75016c5be466b955d64
ahmad <s12112906@stu.najah.edu>
2026-01-14 17:48:53 +0300

    Contact

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import style5 from './Contact.module.css'
+import React from "react";
+import styles from "./Contact.module.css";
+
 export default function Contact() {
   return (
-    <div>
-      <p>hellow</p>
-    </div>
+    <section className={styles.contact}>
+      <h2 className={styles.title}>CONTACT ME</h2>
+
+      <div className={styles.divider}>
+        <span></span>
+        <i className="fa-solid fa-star"></i>
+        <span></span>
+      </div>
+
+      <form className={styles.form}>
+        <input type="text" placeholder="Full name" />
+        <input type="email" placeholder="Email address" />
+        <input type="tel" placeholder="Phone number" />
+        <textarea placeholder="Message"></textarea>
+
+        <button type="submit">Send</button>
+      </form>
+    </section>
   );
 }
