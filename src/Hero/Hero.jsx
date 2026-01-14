695836a287dced516bac8452c12dd1718d523419
ahmad <s12112906@stu.najah.edu>
2026-01-14 17:40:42 +0300

    Portfolio

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import style3 from './Hero.module.css'
+import style from './Hero.module.css';
+import avatar from './../assets/images/avataaars.svg'; 
+
 export default function Hero() {
   return (
-    <div>
-      <p>hellow</p>
-    </div>
+    <section className={style.hero}>
+      <img src={avatar} alt="avatar" className={style.avatar} />
+
+      <h1 className={style.title}>START BOOTSTRAP</h1>
+
+      <div className={style.divider}>
+        <span></span>
+        <i className="fa-solid fa-star"></i>
+        <span></span>
+      </div>
+
+      <p className={style.text}>
+        Graphic Artist - Web Designer - Illustrator
+      </p>
+    </section>
   );
 }
--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -1,7 +1,33 @@
-import style from './portfolio.module.css'
+import style from './Portfolio.module.css';
 
+import img1 from './../assets/images/cabin.png';
+import img2 from './../assets/images/cake.png';
+import img3 from './../assets/images/circus.png';
+import img4 from './../assets/images/game.png';
+import img5 from './../assets/images/safe.png';
+import img6 from './../assets/images/submarine.png';
 
-function Portfolio(){
-<h2>ahmad</h2>
+function Portfolio() {
+  return (
+    <section className={style.portfolio}>
+      <h2 className={style.title}>PORTFOLIO</h2>
+
+      <div className={style.divider}>
+        <span></span>
+        <i className="fa-solid fa-star"></i>
+        <span></span>
+      </div>
+
+      <div className={style.grid}>
+        <img src={img1} alt="portfolio" />
+        <img src={img2} alt="portfolio" />
+        <img src={img3} alt="portfolio" />
+        <img src={img4} alt="portfolio" />
+        <img src={img5} alt="portfolio" />
+        <img src={img6} alt="portfolio" />
+      </div>
+    </section>
+  );
 }
+
 export default Portfolio;
